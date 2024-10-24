8d77ff287b9f4d65a0aee617cddd4368f533c0d1
Arpan-Shah-102 <arpanshah102@gmail.com>
2024-10-24 18:49:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
 let pointer = 0;
-const colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white'];
+const colorList = ['crimson', 'red', 'orange', 'gold', 'yellow', 'limegreen', 'green', 'cyan', 'blue', 'navy', 'indigo', 'purple', 'black', 'grey', 'saddlebrown', 'white'];
 
-document.querySelector('.div > button').addEventListener('click', function () {
+function upperCase(str) {
+  let upperCaseStr = str[0].toUpperCase() + str.slice(1, str.length);
+
+  if (upperCaseStr == 'Limegreen') {
+    return 'Lime';
+  } else if (upperCaseStr == 'Saddlebrown') {
+    return 'Brown';
+  } else if (upperCaseStr == 'White') {
+    return 'White';
+  } else {
+    return upperCaseStr;
+  }
+}
+
+function addEventForBackground() {
   const body = document.querySelector('body');
   body.style.backgroundColor = colorList[pointer];
   body.style.transitionDuration = '0.25s';
 
-  if (pointer == 0 || pointer == 3 || pointer == 4 || pointer == 5 || pointer == 6) {
-    document.querySelector('h1').style.color = 'white';
-    document.querySelector('h4').style.color = 'white';
-  } else if (pointer == 1 || pointer == 2 || pointer ==7) {
-    document.querySelector('h1').style.color = 'black';
-    document.querySelector('h4').style.color = 'black';
+  let textColor;
+
+  if (pointer == 0 || pointer == 1 || pointer == 6 || pointer == 8 || pointer == 9 || pointer == 10 || pointer == 11 || pointer == 12 || pointer == 13 || pointer == 14) {
+    textColor = 'white';
   }
+  else if (pointer == 2 || pointer == 3 || pointer == 4 || pointer == 5 || pointer == 7 || pointer == 15) {
+    textColor = 'black';  
+  }
+  document.querySelector('h1').style.color = textColor;
+  document.querySelector('h4').style.color = textColor;
+  document.querySelector('#currentColor').style.color = textColor;
+
+  document.querySelector('#currentColor').textContent = 'Current Color: '+upperCase(colorList[pointer]);
 
   pointer++;
   if (pointer >= colorList.length) {
     pointer = 0;
   }
+}
+
+document.querySelector('.div > div > button').addEventListener('click', function () {addEventForBackground()});
+
+document.querySelector('#random').addEventListener('click', function () {
+  let randomNumber = Math.floor(Math.random() * colorList.length);
+  while (randomNumber == pointer) {
+    randomNumber = Math.floor(Math.random() * colorList.length);
+  }
+  pointer = randomNumber;
+  
+  addEventForBackground();
 });
 
 for (let i = 0; i <= colorList.length; i++) {
   document.querySelectorAll('.div > p > span')[i].addEventListener('click', function () {
-    const body = document.querySelector('body');
-    body.style.backgroundColor = colorList[i];
-    body.style.transitionDuration = '0.25s';
-
-    pointer = i+1;
-    
-    if (i == 0 || i == 3 || i == 4 || i == 5 || i == 6) {
-      document.querySelector('h1').style.color = 'white';
-      document.querySelector('h4').style.color = 'white';
-    } else if (i == 1 || i == 2 || i ==7) {
-      document.querySelector('h1').style.color = 'black';
-      document.querySelector('h4').style.color = 'black';
-    }
-
-    if (pointer == colorList.length) {
-      pointer = 0;
-    }
+    pointer = i;
+    addEventForBackground();
   });
 }
